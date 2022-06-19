15446d44c3fc5321d9aca4fad5b3d52ab690a204
sravan <bonthasravan@gmail.com>
2022-06-19 21:53:36 +0530

    cpommit after testing

--- js/constants.js
+++ js/constants.js
@@ -1,8 +1,15 @@
-class state{
-    START = false;
-    STOP = true;
-    constructor(start, stop){
-        this.start = start;
-        this.stop = stop;
-    }
-}
+const TIMER = 60;
+
+const dummyTextArray = [
+    "Push yourself, because no one else is going to do it for you.",
+    "Failure is the condiment that gives success its flavor.",
+    "Wake up with determination. Go to bed with satisfaction.",
+    "It's going to be hard, but hard does not mean impossible.",
+    "Learning never exhausts the mind.",
+    "The only way to do great work is to love what you do.",
+    "When we talk about motivating others, the justification is the end result  or what we want to get the person to do.",
+    " How we achieve the end result, are our alternatives.",
+    " As a manager, we need to understand the other person's justification and then come up with alternatives.",
+    " We may then choose the right alternative.",
+    " However, in general, we choose the first or the emotionally satisfying one."
+];
--- js/typeSpeedCalculator.js
+++ js/typeSpeedCalculator.js
@@ -1,35 +1,141 @@
 let errorContainer = document.getElementById("errorContent");
 let timerContainer = document.getElementById("timerContent");
 let accuracyContainer = document.getElementById("accuracyContent");
-
+let dummyTextContainer = document.getElementById("dummyText");
+let wpmContainer = document.getElementById("wpmContainer");
+let cpmContainer = document.getElementById("cpmContainer");
+let wpmContent = document.getElementById("wpmContent");
+let cpmContent = document.getElementById("cpmContent");
 let inputText = document.getElementById("inputText");
-//let accuracyContainer = document.getElementById("accuracyContent");
+let restartButton = document.getElementById("restart");
 
-let timer = 7;
-timerContainer.innerHTML = timer+"s";
+let timeElapsed = 0;
+let timeLeft = TIMER;
+let total_errors = 0;
+let errors = 0;
+let charactersTyped = 0;
+let textIndex = 0;
+let timer = null;
+let dummyText = "";
 
-inputText.addEventListener("keydown",()=>{
-    setInterval(() => {
-    if(timer>0){
-        timer--;
-        timerContainer.innerHTML = timer+"s";
-    }
-    else{   
-        clearInterval(timerFunction);
-        alert("Times up");
-    }
-},1000);
-})
-/*
-var timerFunction = setInterval(() => {
-    if(timer>0){
-        timer--;
-        timerContainer.innerHTML = timer+"s";
-    }
-    else{   
-        clearInterval(timerFunction);
-        alert("Times up");
-    }
-},1000);
-*/
+timerContainer.innerHTML = timeLeft + "s";
+wpmContainer.style.display = "none";
+cpmContainer.style.display = "none";
+restartButton.style.display = "none";
 
+const timerFunction = () => {
+    if (timeLeft > 0) {
+        timeLeft--;
+        timeElapsed++;
+        timerContainer.innerHTML = timeLeft + "s";
+    } else {
+        //        alert("Times up");
+        stopGame();
+        return;
+
+    }
+};
+
+const resetValues = () => {
+    timeLeft = TIMER;
+    timeElapsed = 0;
+    inputText.value = "";
+    errorContainer.innerHTML = 0;
+    accuracyContainer.innerHTML = 100;
+
+    wpmContainer.style.display = "none";
+    cpmContainer.style.display = "none";
+    restartButton.style.display = "none";
+    inputText.readOnly = false;
+    errors = 0;
+    total_errors = 0;
+    charactersTyped = 0;
+    textIndex = 0;
+    dummyTextContainer.innerHTML = "Click on the area below to start the game";
+}
+
+const startGame = () => {
+    resetValues();
+    updateDummyText();
+    clearInterval(timer);
+    timer = setInterval(timerFunction, 1000)
+
+}
+
+const updateDummyText = () => {
+    dummyTextContainer.innerHTML = null;
+    dummyText = dummyTextArray[textIndex];
+    dummyText.split('').forEach(char => {
+        const charSpan = document.createElement('span');
+        charSpan.innerText = char;
+        dummyTextContainer.appendChild(charSpan);
+    })
+    if (textIndex < dummyTextArray.length - 1) {
+        textIndex++;
+    } else {
+        textIndex = 0;
+    }
+}
+
+const validateInputText = () => {
+    charactersTyped++;
+    errors = 0;
+    let inputCharArray = inputText.value.split("");
+    let dummyTextSpanArray = dummyTextContainer.querySelectorAll('span');
+    dummyTextSpanArray.forEach((char, index) => {
+        let inputChar = inputCharArray[index];
+        if (inputChar == null || inputChar == "") {
+            char.classList.remove('correctChar');
+            char.classList.remove('incorrectChar');
+        } else if (inputChar == char.innerHTML) {
+            char.classList.add('correctChar');
+            char.classList.remove('incorrectChar');
+        } else {
+            char.classList.add('incorrectChar');
+            char.classList.remove('correctChar');
+            errors++;
+        }
+
+    });
+    errorContainer.innerHTML = total_errors + errors;
+    let correctCharacters = (charactersTyped - (total_errors + errors));
+    let accuracy = ((correctCharacters / charactersTyped) * 100);
+    accuracyContainer.innerHTML = Math.round(accuracy);
+
+    if (inputText.value.length == dummyText.length) {
+        updateDummyText();
+
+        total_errors += errors;
+
+        inputText.value = "";
+    }
+}
+
+const wordCounter = () => {
+
+    let wordCount = Math.round((((charactersTyped / 5) / timeElapsed) * 60));
+    wpmContent.innerHTML = wordCount;
+
+}
+
+const characterCounter = () => {
+    let charCount = Math.round(((charactersTyped / timeElapsed) * 60));
+    cpmContent.innerHTML = charCount;
+
+}
+
+const stopGame = () => {
+    clearInterval(timer);
+    wordCounter();
+    characterCounter();
+    wpmContainer.style.display = "";
+    cpmContainer.style.display = "";
+    restartButton.style.display = "";
+    inputText.readOnly = true;
+    inputText.innerHTML = "Click on the area below to start the game";
+
+}
+
+inputText.addEventListener("focus", startGame);
+inputText.addEventListener("input", validateInputText);
+restartButton.addEventListener("click", resetValues);
