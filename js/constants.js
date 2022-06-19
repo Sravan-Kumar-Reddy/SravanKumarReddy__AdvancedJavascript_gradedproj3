a57a91055a86a9fa2139c85a0accd859de613b0d
sravan <bonthasravan@gmail.com>
2022-06-19 21:54:47 +0530

    format changes

--- js/constants.js
+++ js/constants.js
@@ -8,8 +8,8 @@ const dummyTextArray = [
     "Learning never exhausts the mind.",
     "The only way to do great work is to love what you do.",
     "When we talk about motivating others, the justification is the end result  or what we want to get the person to do.",
-    " How we achieve the end result, are our alternatives.",
-    " As a manager, we need to understand the other person's justification and then come up with alternatives.",
-    " We may then choose the right alternative.",
-    " However, in general, we choose the first or the emotionally satisfying one."
+    "How we achieve the end result, are our alternatives.",
+    "As a manager, we need to understand the other person's justification and then come up with alternatives.",
+    "We may then choose the right alternative.",
+    "However, in general, we choose the first or the emotionally satisfying one."
 ];
